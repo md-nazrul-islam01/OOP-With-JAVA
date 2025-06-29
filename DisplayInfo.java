5683156fba63725d7f1ae81ab4ddeeaec61a64af
Md. Nazrul Islam <e243032@ugrad.iiuc.ac.bd>
2025-06-30 01:52:34 +0600

    Add Lab 1 files

---
+++ DisplayInfo.java
@@ -0,0 +1,16 @@
+public class DisplayInfo{
+    public static void main(String[]a){
+        String n="Nazrul";
+        String id="E-243032";
+        int age=20;
+        String mail="mdnazrulislamrabby@gmail.com";
+        String ph="01672443556";
+        String add="Bayezid, Chittagong";
+        System.out.println("Name:"+n);
+        System.out.println("ID:"+id);
+        System.out.println("Age:"+age);
+        System.out.println("Email:"+mail);
+        System.out.println("Phone:"+ph);
+        System.out.println("Address:"+add);
+    }
+}
